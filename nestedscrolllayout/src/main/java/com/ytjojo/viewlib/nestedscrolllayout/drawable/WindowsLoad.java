24c9da27692713fa7b97c3c07690b7ab632689d9
ytjojo <ytjojo@163.com>
2017-05-22 22:00:19 +0800

    WindowsLoad

--- nestedscrolllayout/src/main/java/com/ytjojo/viewlib/nestedscrolllayout/drawable/WindowsLoad.java
+++ nestedscrolllayout/src/main/java/com/ytjojo/viewlib/nestedscrolllayout/drawable/WindowsLoad.java
@@ -4,8 +4,10 @@ import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.PointF;
 import android.graphics.Rect;
+import android.support.v4.view.animation.PathInterpolatorCompat;
 import android.view.animation.Interpolator;
 
 import com.ytjojo.viewlib.nestedscrolllayout.Utils;
@@ -24,10 +26,18 @@ public class WindowsLoad extends LoadingDrawable {
     private ValueAnimator circleAnimator2;
     private ValueAnimator circleAnimator3;
     private float mTrajectoryRadius;
+    private int mDuration =1000;
+    private int mDelay = 80;
 
 
     public WindowsLoad(Context context) {
         mRadiu = Utils.dip2px(context,3);
+        if(mPathInterpolatorCompat == null){
+            Path path =new Path();
+            path.cubicTo(0.8f, 0.2f, 0.2f, 0.8f, 1f, 1f);
+//            path.cubicTo(0.81f, 0.11f, 0.19f , 0.89f, 1f, 1f);
+            mPathInterpolatorCompat = PathInterpolatorCompat.create(path);
+        }
     }
 
 
@@ -54,15 +64,22 @@ public class WindowsLoad extends LoadingDrawable {
             canvas.drawCircle(mPointFs.get(i).x, mPointFs.get(i).y, mRadiu, paint);
         }
     }
+    float padding;
 
     @Override
     public void onReset() {
-        start1 = new float[]{getWidth() / 2, 2 * mRadiu};
+        start1 = new float[]{getWidth() / 2, padding};
+//        start2 = onCiecleCoordinate(-(2*pi -delay(mDuration,mDelay)), start1, circleCentre);
+//        start3 = onCiecleCoordinate(-(2*pi -delay(mDuration,mDelay*2)), start2, circleCentre);
         start2 = onCiecleCoordinate(-0.25f, start1, circleCentre);
         start3 = onCiecleCoordinate(-0.25f, start2, circleCentre);
         mPointFs.get(0).set(start1[0],start1[1]);
         mPointFs.get(1).set(start2[0],start2[1]);
         mPointFs.get(2).set(start3[0],start3[1]);
+        mStartLocations = new ArrayList<>();
+        mStartLocations.add(start1);
+        mStartLocations.add(start2);
+        mStartLocations.add(start3);
     }
 
     @Override
@@ -75,20 +92,18 @@ public class WindowsLoad extends LoadingDrawable {
                 mPointFs.add(new PointF());
             }
         }
-        mTrajectoryRadius = Math.min(getHeight(),getWidth())/2 -2*mRadiu;
+        padding = 4 * mRadiu;
+        mTrajectoryRadius = Math.min(getHeight(),getWidth())/2 -padding;
         circleCentre = new float[]{getWidth() / 2, getHeight() / 2};
-        start1 = new float[]{getWidth() / 2, 2*mRadiu};
-        start2 = onCiecleCoordinate(-0.25f, start1, circleCentre);
-        start3 = onCiecleCoordinate(-0.25f, start2, circleCentre);
-        mPointFs.get(0).set(start1[0],start1[1]);
-        mPointFs.get(1).set(start2[0],start2[1]);
-        mPointFs.get(2).set(start3[0],start3[1]);
-        mStartLocations = new ArrayList<>();
-        mStartLocations.add(start1);
-        mStartLocations.add(start2);
-        mStartLocations.add(start3);
+       onReset();
     }
 
+    public float delay(long duration,long delay){
+        float t = 1- 1f*delay/duration;
+        float angle =  2* pi *(mPathInterpolatorCompat.getInterpolation(t));
+
+        return angle;
+    }
 
     @Override
     public ArrayList<ValueAnimator> onCreateAnimators() {
@@ -96,7 +111,7 @@ public class WindowsLoad extends LoadingDrawable {
         for (int i = 0; i < 3; i++) {
             final int index=i;
             final ValueAnimator valueAnimator =
-                    getCircleData(mStartLocations.get(i), circleCentre, i*80);
+                    getCircleData(mStartLocations.get(i), circleCentre, i*mDelay);
             addUpdateListener(valueAnimator,new ValueAnimator.AnimatorUpdateListener() {
                 @Override
                 public void onAnimationUpdate(ValueAnimator animation) {
@@ -116,15 +131,15 @@ public class WindowsLoad extends LoadingDrawable {
 
     }
 
-
-    private SlowToQuick slowToQuick = new SlowToQuick();
+    Interpolator mPathInterpolatorCompat;
 
     private ValueAnimator getCircleData(float[] startCoordinate, float[] RCoordinate, int delay) {
         float x1 = startCoordinate[0];
         float y1 = startCoordinate[1];
         float x0 = RCoordinate[0];
         float y0 = RCoordinate[1];
-        circleR = (float) Math.sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0));
+//        circleR = (float) Math.sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0));
+        circleR = mTrajectoryRadius;
         float param = (float) (Math.abs(y1 - y0) / circleR);
         if (param < -1.0) {
             param = -1.0f;
@@ -142,10 +157,12 @@ public class WindowsLoad extends LoadingDrawable {
             a = 2 * pi - a;
         }
         ValueAnimator circleAnimator = ValueAnimator.ofFloat(a, a + 2 * pi);
-        circleAnimator.setDuration(1000);
-        circleAnimator.setInterpolator(slowToQuick);
+        circleAnimator.setDuration(mDuration);
 //        circleAnimator.setInterpolator(new EaseCubicInterpolator(0.31f, 0.85f,0.77f, 0.14f));
-        circleAnimator.setInterpolator(new HesitateInterpolator());
+
+//        path.lineTo(1f, 1f);
+
+        circleAnimator.setInterpolator(mPathInterpolatorCompat);
         circleAnimator.setRepeatCount(-1);
         circleAnimator.setStartDelay(delay);
         return circleAnimator;
@@ -180,12 +197,7 @@ public class WindowsLoad extends LoadingDrawable {
         return new float[]{x, y};
     }
 
-    class SlowToQuick implements Interpolator {
-        @Override
-        public float getInterpolation(float input) {
-            return input * input;
-        }
-    }
+
     class HesitateInterpolator implements Interpolator {
 
         private static final double POW = 1.0/2.0;
@@ -197,4 +209,5 @@ public class WindowsLoad extends LoadingDrawable {
                     : (float) Math.pow((1 - input) * 2, POW) * -0.5f + 1;
         }
     }
+
 }
